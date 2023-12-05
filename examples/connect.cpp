1ba45a0628630169585026693d6f1ec9a602553f
Daniil Rozanov <daniilrozzanov@gmail.com>
2023-12-06 01:31:56 +0300

    Fixed example

--- examples/connect.cpp
+++ examples/connect.cpp
@@ -1,10 +1,10 @@
 #include "ftp/handshake_params.hpp"
-#include <ftp/connection.hpp>
+#include <ftp/details/connection_base.hpp>
 
 int main()
 {
   boost::asio::io_context ioc;
-  ftp::connection conn(ioc);
+  ftp::detail::connection_base conn(ioc.get_executor());
   boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string("192.168.1.66"), 21);
   ftp::handshake_params params("ftpuser", "ftpuser");
   conn.connect(ep, params);
