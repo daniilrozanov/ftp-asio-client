1089cc7862fa0166bde28f71e9f1030c194861cf
Daniil Rozanov <daniilrozzanov@gmail.com>
2023-12-08 01:47:43 +0300

    Command list made as enum, added some supporting functions

---
+++ include/ftp/command.hpp
@@ -0,0 +1,73 @@
+#ifndef FTP_COMMAND_HPP
+#define FTP_COMMAND_HPP
+
+#include <string_view>
+
+namespace ftp {
+
+enum class cmd_group
+{
+  unknown = 0,
+  login,
+  logout,
+  transfer_parameters,
+  file_action,
+  informational,
+  miscellaneous
+};
+
+enum class cmd
+{
+  unknown = 0,
+  // login
+  user,
+  pass,
+  acct,
+  cwd,
+  cdup,
+  smnt,
+  // logout
+  quit,
+  rein,
+  // transfer parameters
+  port,
+  pasv,
+  type,
+  stru,
+  mode,
+  // file action commands
+  retr,
+  stor,
+  stou,
+  appe,
+  allo,
+  rest,
+  rnfr,
+  rnto,
+  abor,
+  dele,
+  rmd,
+  mkd,
+  pwd,
+  list,
+  // informational commands
+  syst,
+  stat,
+  help,
+  // miscellaneous commands
+  site,
+  noop
+};
+
+constexpr std::string_view to_string(cmd c);
+
+constexpr cmd string_to_cmd(std::string_view c);
+
+constexpr cmd_group group_of_cmd(cmd c);
+
+}  // namespace ftp
+
+#ifdef FTP_HEADER_ONLY
+#include <ftp/impl/command.cpp>
+#endif  // FTP_HEADER_ONLY
+#endif  // !FTP_COMMAND_HPP
---
+++ include/ftp/impl/command.cpp
@@ -0,0 +1,105 @@
+#ifndef FTP_COMMAND_CPP
+#define FTP_COMMAND_CPP
+
+#pragma once
+
+#include <ftp/command.hpp>
+
+namespace ftp {
+
+constexpr std::string_view to_string(cmd c)
+{
+  using namespace std::string_view_literals;
+  switch (c)
+  {
+  case cmd::unknown: return "<unknown>"sv;
+  case cmd::user: return "USER"sv;
+  case cmd::pass: return "PASS"sv;
+  case cmd::acct: return "ACCT"sv;
+  case cmd::cwd: return "CWD"sv;
+  case cmd::cdup: return "CDUP"sv;
+  case cmd::smnt: return "SMNT"sv;
+  case cmd::quit: return "QUIT"sv;
+  case cmd::rein: return "REIN"sv;
+  case cmd::port: return "PORT"sv;
+  case cmd::pasv: return "PASV"sv;
+  case cmd::type: return "TYPE"sv;
+  case cmd::stru: return "STRU"sv;
+  case cmd::mode: return "MODE"sv;
+  case cmd::retr: return "RETR"sv;
+  case cmd::stor: return "STOR"sv;
+  case cmd::stou: return "STOU"sv;
+  case cmd::appe: return "APPE"sv;
+  case cmd::allo: return "ALLO"sv;
+  case cmd::rest: return "REST"sv;
+  case cmd::rnfr: return "RNFR"sv;
+  case cmd::rnto: return "RNTO"sv;
+  case cmd::abor: return "ABOR"sv;
+  case cmd::dele: return "DELE"sv;
+  case cmd::rmd: return "RMD"sv;
+  case cmd::mkd: return "MKD"sv;
+  case cmd::pwd: return "PWD"sv;
+  case cmd::list: return "LIST"sv;
+  case cmd::site: return "SITE"sv;
+  case cmd::syst: return "SYST"sv;
+  case cmd::stat: return "STAT"sv;
+  case cmd::help: return "HELP"sv;
+  case cmd::noop: return "NOOP"sv;
+  }
+  return "";
+}
+
+constexpr cmd string_to_cmd(std::string_view c)
+{
+  using namespace std::string_view_literals;
+  return cmd::unknown;
+}
+
+constexpr cmd_group group_of_cmd(cmd c)
+{
+  switch (c)
+  {
+  case cmd::user:
+  case cmd::pass:
+  case cmd::acct:
+  case cmd::cwd:
+  case cmd::cdup:
+  case cmd::smnt: return cmd_group::login;
+
+  case cmd::quit:
+  case cmd::rein: return cmd_group::logout;
+
+  case cmd::port:
+  case cmd::pasv:
+  case cmd::type:
+  case cmd::stru:
+  case cmd::mode: return cmd_group::transfer_parameters;
+
+  case cmd::retr:
+  case cmd::stor:
+  case cmd::stou:
+  case cmd::appe:
+  case cmd::allo:
+  case cmd::rest:
+  case cmd::rnfr:
+  case cmd::rnto:
+  case cmd::abor:
+  case cmd::dele:
+  case cmd::rmd:
+  case cmd::mkd:
+  case cmd::pwd:
+  case cmd::list: return cmd_group::file_action;
+
+  case cmd::syst:
+  case cmd::stat:
+  case cmd::help: return cmd_group::informational;
+
+  case cmd::site:
+  case cmd::noop: return cmd_group::miscellaneous;
+
+  case cmd::unknown: return cmd_group::unknown;
+  }
+}
+}  // namespace ftp
+
+#endif  // !FTP_COMMAND_CPP
